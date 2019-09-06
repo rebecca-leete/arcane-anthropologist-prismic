1186728c113f16407bd25fa3e99f220f73a27bbf
Rebecca Leete <rebecca072@live.com>
2019-09-06 14:32:54 +1200

    set up with prismic and basic homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div>
+  <div style={{display:'flex', backgroundColor:'#405926'}}>The
+    <h1> ArcaneAnthropologist</h1>
+    <p style={{padding:'20px'}}>Header goes heeeeeere</p>
+  </div>
+  <div style={{width:'100%', height:'600px', backgroundColor:'#A8B39C', display:'flex', justifyContent:'center'}}>
+    <img style={{margin:'auto'}} src="https://nimentrix.files.wordpress.com/2018/12/chiara-bautista-traveling-subway-wolf.jpg" alt="rabbit girl and starwolf"/></div>
+  <div style={{height:'600px', border:'solid black 1pt'}}><h1 style={{textAlign:'center', paddingTop:'300px'}}>What is this space?? Intro</h1></div>
+  <div style={{height:'600px', border:'solid black 1pt'}}><h1 style={{textAlign:'center', paddingTop:'300px'}}>Who is this person?? Intro</h1></div>
+  <div style={{height:'100px', backgroundColor:'#405926'}}><h3 style={{textAlign:'center', paddingTop:'20px'}}>Footerrrrrrrrrrr</h3></div>
+</div>
